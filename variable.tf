04e3e0f836d2c73804c1869bc6d57e3bfd4a5ee9
thebestlearner-ric <watto1990@gmail.com>
2022-11-06 23:18:39 +0800

    ansible roles added

--- variable.tf
+++ variable.tf
@@ -1,12 +1,12 @@
 variable "aws_access_key" {
-  default = "ASIATIDU7EBLCHGBTSNS"
+  default = "ASIATIDU7EBLJVV26VPR"
 }
 variable "aws_secret_key" {
-  default = "Ka6nmEpFy8qjcZp+ivQBj7OMzbdY/YoQDiCgaxeI"
+  default = "4r6taawtQwxNzaf77aIpqRpT/TXGxHzh/t7lhHHZ"
 }
 variable "region" {
   default = "us-east-1"
 }
 variable "token" {
-  default = "FwoGZXIvYXdzEIn//////////wEaDDQyurtQlGS7bgPmESK3AZyxsZqiCp2w88ZBN4tt9D+jzIqKYXsKAFogQjJ+ZL4eosKRZ0NNX/1sietVLQz5aXbQuzQUGpercKAVfi4TDILOvjRad/o90FiHkkFYh75yxXPaRP/r8Xs3pcI0ob6Rm6sVUMFswySdPgawQJreFREXhCJit1W0Mlo8oP03htD9b9aYSS+Wf5pO6BnUAVPz8gouJlLQkPhlmm7Tlbw2LXDJ4GW3iZ8fFlw3ivH+ZwarxXxTHEDOYijGqPqaBjItWU+za/WjDyUOEYFg3A1RyJdyvPs7XxZ+/Md6rcC53dhieqr6U3PIvr09KEvq"
+  default = "FwoGZXIvYXdzEKD//////////wEaDCQaCdOXE/PH8OcRkyK3AWzUuOqxxfPajXaXu4/dDmX8/D4pmH6FlaO3gYfhUZ2MFz7p3+yOvYhgvmw8sN5V8o3Y7r0aWKoInCCNU4h3G8h2qfmI6ubVu4KbpfPlqUn0bnfpNLj2baMuszCM1sN8h7meSPMi8r56tti7edkzQUTr1PhM97Iq520JxS/jxK00UydZkK5/4lQ06Jw+mkxtjw/FJjdtRL4ZIUGFh+WWUxLA9frtGmGra8U6qUxlBG2EdWmWPA47CSiks/+aBjItUFFG/lp9fRdxMidv6/Uw6m5gpub+OCnayfUdnO7PV44Pb/+F3plTxLmzPvo8"
 }
